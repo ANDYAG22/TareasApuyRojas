c3e817605c9190dbf089b2e8c58eb2a0f22ae396
ANDYAG22 <148302210+ANDYAG22@users.noreply.github.com>
2024-08-01 13:51:43 -0600

    Codigo con errores de Flake8
    Se sube el codigo con 3 errores identificables por flake8

---
+++ Tarea 1/pruebas_flake8.py
@@ -0,0 +1,11 @@
+# Funcion que muestra holaMundo cuando variable=14
+def holaMundo ():
+    variable = 14;
+    if (variable == 14):  # Verifica si variable es igual a 14
+        print("Hola Mundo")  # Imprime en terminal "hola mundo"
+    else:
+        # Imprime en terminal el valor que debe tener variable
+        print("La variable es: ", variable, " pero debe de tener un valor de 14")
+
+
+holaMundo()
